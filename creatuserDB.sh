0395a3613161b3e8ae90c304881555ca411d6b27
boujeh <mohamed.boujeh@esprit.tn>
2021-09-23 16:29:36 +0200

    correction du com

--- creatuserDB.sh
+++ creatuserDB.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-echo "Bonjour"
 
-#today=$(date +'%m/%d/%Y')
+today=$(date +'%m/%d/%Y')
 #echo "$today"
-#email=$1
-#user=$2
-#uid=$3	
-#DB_USER=postgres
-#DB_PWD=Boujeh123
-#DB_SERVER=192.168.1.2
-#DB_NAME=postgres
-#PGPASSWORD="$DB_PWD" psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_SERVER" <<END_OF_SQL	
-#	INSERT INTO newUsers (email, username, uid, day) VALUES ('{$email}', '{$user}', '{$uid}', '{$today}');	
-#END_OF_SQL
-#	echo "after database connection"
+email=$1
+user=$2
+uid=$3	
+DB_USER=postgres
+DB_PWD=Boujeh123
+DB_SERVER=192.168.1.2
+DB_NAME=postgres
+PGPASSWORD="$DB_PWD" psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_SERVER" <<END_OF_SQL	
+	INSERT INTO newUsers (email, username, uid, day) VALUES ('{$email}', '{$user}', '{$uid}', '{$today}');	
+END_OF_SQL
+	echo "after database connection"
